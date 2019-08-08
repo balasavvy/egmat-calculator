107733278ee490e3c22d381fad76e7bce64d8677
balachandar <balachandar.durairaj@gmail.com>
2019-08-08 09:37:14 +0530

    gmat layout updated

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { GmatScoreComponent } from './gmat-score/gmat-score.component';
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home',  component: HomeComponent }
+  { path: 'home',  component: HomeComponent },
+  { path: 'gmat-score',  component: GmatScoreComponent },
+  {
+    path: "**",
+    redirectTo: "/home"
+  }
 ]
 @NgModule({
   declarations: [],
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -6,21 +6,37 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { LayoutRoutingModule } from './layout-routing.module';
 import { SliderComponent } from './home/slider/slider.component';
-
+import { GmatScoreComponent } from './gmat-score/gmat-score.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule, HttpClient, HTTP_INTERCEPTORS, HttpHeaders } from '@angular/common/http';
 @NgModule({
   declarations: [
     LayoutComponent,
     HomeComponent,
     HeaderComponent,
     FooterComponent,
-    SliderComponent
+    SliderComponent,
+    GmatScoreComponent
   ],
   exports: [
     LayoutComponent
   ],
   imports: [
     CommonModule,
-    LayoutRoutingModule
+    BrowserModule,
+   // HttpModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    LayoutRoutingModule,
+     BrowserAnimationsModule, // required animations module
+    // ToastrModule.forRoot({
+    //   timeOut: 2000,
+    //   positionClass: 'toast-top-right',
+    //   preventDuplicates: true,
+    // })
   ]
 })
 export class LayoutModule { }
