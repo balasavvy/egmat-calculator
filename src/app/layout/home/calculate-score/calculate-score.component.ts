eb6440449d8e974316d5877647754c4445234f34
balachandar <balachandar.durairaj@gmail.com>
2019-08-10 06:45:47 +0530

    progressBar in progress

---
+++ src/app/layout/home/calculate-score/calculate-score.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculate-score',
+  templateUrl: './calculate-score.component.html',
+  styleUrls: ['./calculate-score.component.css']
+})
+export class CalculateScoreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
